60284e0a2423dbcfd679967ec5ccc16e431cc1ea
AnastasiaLL <lukyanchik.nastya@bk.ru>
2022-05-19 17:14:26 +0300

    fix:add new solution to 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,53 +1,24 @@
-const fs = require ('fs');
-const path = require ('path');
-const process = require('process');
 
-const textWay = path.join(__dirname, 'text.txt');
-// let writeableStream = fs.createWriteStream(textWay);
+let path = require('path');
+let fs = require('fs');
+let textWay = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+let { stdout, stdin, exit } = require('process');
 
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-readline.question('Привет! напиши мне пару строчек ', (name) => {
-    if (name === 'exit'){ 
-        // process.stdout.write('пока!')
-        readline.close()
-    } else {
-
-        fs.appendFile(textWay, `${name}\n`, err=>{
-            if (err){throw err}
-            readline.close()
-
-        })
-    }
-
+stdout.write('напиши что-нибудь: ');
+stdin.on('data', (data) => {
+    
+    if (data.toString().trim() === 'exit'){ 
+        bye ()
+    }    
+    textWay.write(data)
     
 })
 
-process.on('exit', ()=>{
-    process.stdout.write('\nbye!\n')
-})
+process.on('SIGINT', bye);
 
 
+function bye (){
+    stdout.write('\nbye!\n')
+   exit();
+}
 
-
-
-
-// output.write('введите 1е')
-// readline.on('line', (value) => {
-//     if (value === 'exit'){
-//         readline.close();
-//     }else {
-//         output.write('2e: ')
-//         let str = value.toString();
-//         textWay.write(`${str}\n`)
-//     }
-// })
-
-// process.on('exit', ()=>{
-//   console.log('\nbye!\n')
-//   output.write('clouse')
-// })
